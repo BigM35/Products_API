ec5817c860f421b836fb5dab4422573f185b7f57
Manny M <bigm@Mannys-MacBook-Pro.local>
2022-03-15 11:48:25 -0500

    add picture colum + created serializer file and class + updated model class to inclube an image and makemigration and migrate change to reflect in data base

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from pyexpat import model
 from django.db import models
 
@@ -8,3 +9,4 @@ class Product(models.Model):
     description = models.CharField(max_length = 255)
     price = models.DecimalField(max_digits = 5, decimal_places = 2)
     inventory_quantity = models.IntegerField()
+    picture = models.ImageField(upload_to=None, height_field=5, width_field=5, max_length=100)

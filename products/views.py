c0556efa9b8fd63c924f7e5f1081d94bc6122efa
Manny M <bigm@Mannys-MacBook-Pro.local>
2022-03-15 13:49:47 -0500

    create url file in the app folder and set up the url to work for the GET response + create the GET function and successfully use it to communicate with database

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
 from logging import raiseExceptions
 from django.shortcuts import get_object_or_404
 from http.client import FOUND
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Car
-from .serializers import CarSerializer
-from cars import serializers
+from .models import Product
+from .serializers import ProductSerializer
+from products import serializers
 
 # Create your views here.
+@api_view(["GET"])
+def product_list(request):
+    product = Product.objects.all()
+    serializer = ProductSerializer(product, many=True)
+
+    return Response("serializer")

1a21fd4787bda547befe126e80b709df0430dbcd
Manny M <bigm@Mannys-MacBook-Pro.local>
2022-03-15 16:40:46 -0500

    add POST functionality + edited the image field to take blank and null so i can test without having to post a picture everytime by making changes to the model and the serializer object logic

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from email.mime import image
 from pyexpat import model
+from tkinter.tix import IMAGE
 from django.db import models
 
 # Create your models here.
@@ -9,4 +10,4 @@ class Product(models.Model):
     description = models.CharField(max_length = 255)
     price = models.DecimalField(max_digits = 5, decimal_places = 2)
     inventory_quantity = models.IntegerField()
-    picture = models.ImageField(upload_to=None, height_field=5, width_field=5, max_length=100)
+    product_image = models.ImageField(upload_to='images/', null=True, blank=True, max_length=100)
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from logging import raiseExceptions
+from pickle import PUT
 from django.shortcuts import get_object_or_404
 from http.client import FOUND
 from rest_framework.decorators import api_view
@@ -9,9 +10,23 @@ from .serializers import ProductSerializer
 from products import serializers
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(["GET", 'POST'])
 def product_list(request):
-    product = Product.objects.all()
-    serializer = ProductSerializer(product, many=True)
+    if request.method == 'GET':
+        product = Product.objects.all()
+        serializer = ProductSerializer(product, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    return Response("serializer")
+
+'''@api_view(["GET", 'PUT'])
+def single_product(request, pk):
+    if request == 'GET':
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request == 'PUT':'''

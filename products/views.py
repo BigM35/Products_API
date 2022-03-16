943dd7d2f8dab98cad36281c65acc5722ce67602
Manny M <bigm@Mannys-MacBook-Pro.local>
2022-03-15 21:47:10 -0500

    complete PUT functionality+ completed DELETE functionality + update URLs path for the newly created CRUD functions with PK information (Get,Put,Delete)

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from ast import Delete
 from logging import raiseExceptions
 from pickle import PUT
 from django.shortcuts import get_object_or_404
@@ -23,10 +24,18 @@ def product_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-'''@api_view(["GET", 'PUT'])
-def single_product(request, pk):
-    if request == 'GET':
-        product = Product.objects.get(pk=pk)
+@api_view(["GET", 'PUT', 'DELETE'])
+def select_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    elif request == 'PUT':'''
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':  
+        product.Delete() 
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
